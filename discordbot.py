f3ecb58c46776bed082a49f843f2ce263985e113
C David Rubiano <cdavidrubiano@gmail.com>
2022-07-01 12:38:42 -0400

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,9 +8,9 @@ target_token = os.environ['TARGET_DISCORD_BOT_TOKEN']
 
 #m
 source_channel_id = int(os.environ['SOURCE_CHANNEL_ID'])
-source_channel_id = int(os.environ['SOURCE_CHANNEL_ID_2'])
-source_channel_id = int(os.environ['SOURCE_CHANNEL_ID_3'])
-source_channel_id = int(os.environ['SOURCE_CHANNEL_ID_4'])
+source_channel_id_2 = int(os.environ['SOURCE_CHANNEL_ID_2'])
+source_channel_id_3 = int(os.environ['SOURCE_CHANNEL_ID_3'])
+source_channel_id_4 = int(os.environ['SOURCE_CHANNEL_ID_4'])
 
 target_channel_id = int(os.environ['TARGET_CHANNEL_ID'])
 
@@ -42,7 +42,7 @@ def build_embed(author_name, author_picture, embed_desc, embed_color, embed_imag
 
 @source_client.event
 async def on_message(message):
-    if message.channel.id == source_channel_id:
+    if message.channel.id == source_channel_id or message.channel.id == source_channel_id_2 or message.channel.id == source_channel_id_3 or message.channel.id == source_channel_id_4:
         author_name = message.author.name + "#" + message.author.discriminator
         if len(message.attachments) > 0:
             image_url = message.attachments[0].url
